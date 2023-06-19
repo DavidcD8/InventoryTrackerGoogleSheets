18b19c45339e4b029a9d7f14daade24dd7d9f269
DavidcD8 <>
2023-06-19 00:02:10 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,32 +1,97 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
-Welcome,
+# Inventory Tracker
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **March 14, 2023**
+This is a Python code for an Inventory Tracker that uses Google Sheets to store and manage item data. The code provides functionality to connect to a Google Sheets document, create an inventory table, insert new items, search for items, update item quantities, restock items
 
-## Reminders
+## Prerequisites
 
-- Your code must be placed in the `run.py` file
-- Your dependencies must be placed in the `requirements.txt` file
-- Do not edit any of the other files or your code may not deploy properly
+Before running the code, ensure that you have the following:
 
-## Creating the Heroku app
+- Python 3.x installed on your system.
+- The necessary Python packages installed, including `gspread` and `google-auth`.
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+## Getting Started
 
-1. `heroku/python`
-2. `heroku/nodejs`
+1. Make sure you have a Google Sheets document created for your inventory.
+2. Create a service account and download the service account key file (JSON format) from the Google Cloud Console.
+3. Rename the service account key file to `credentials.json` and place it in the same directory as the Python script.
+4. Open the `inventory_tracker.py` file and modify the `DOCUMENT_NAME` constant to match your Google Sheets document name.
+5. Run the Python script using `python inventory_tracker.py`.
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+## Usage
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+1. Upon running the code, the program will display a menu with the following options:
+   - Search for an item
+   - Insert a new item
+   - Update quantity
+   - Restock item
+   - Exit
 
-Connect your GitHub repository and deploy as normal.
+2. Follow the prompts and enter the appropriate information for each operation.
 
-## Constraints
+## Example
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+Choose an operation:
 
----
+1.  Search for an item
+2.  Insert a new item
+3.  Update quantity
+4.  Restock item
+5.  Exit Enter your choice (1, 2, 3, 4, or 0): 2 Enter item model: Laptop Enter initial quantity: 5 Item added successfully.
 
-Happy coding!
+Choose an operation:
+
+1.  Search for an item
+2.  Insert a new item
+3.  Update quantity
+4.  Restock item
+5.  Exit Enter your choice (1, 2, 3, 4, or 0): 1 Enter the item model or shorthand code to search: Laptop Matching items: Item: Laptop, Quantity: 5
+
+Choose an operation:
+
+1.  Search for an item
+2.  Insert a new item
+3.  Update quantity
+4.  Restock item
+5.  Exit Enter your choice (1, 2, 3, 4, or 0): 3 Enter the item model to update quantity: Laptop Enter the quantity sold during the daily sale: 2 Quantity updated for Laptop: 3
+
+Choose an operation:
+
+1.  Search for an item
+2.  Insert a new item
+3.  Update quantity
+4.  Restock item
+5.  Exit Enter your choice (1, 2, 3, 4, or 0): 4 Enter the item model to restock: Laptop Enter the additional quantity to be added: 10 Item restocked successfully. Matching items: Item: Laptop, Quantity: 13
+
+Choose an operation:
+
+1.  Search for an item
+2.  Insert a new item
+3.  Update quantity
+4.  Restock item
+5.  Exit Enter your choice (1, 2, 3, 4, or 0): 0 Exiting the inventory tracking application.
+
+
+## Database Structure
+
+The inventory is stored in a Google Sheets document with the following columns:
+- Item Model
+- Quantity
+
+## Note
+
+- When searching for an item, you can enter either the item model or a shorthand code. The application will return matching items based on the input.
+- When inserting a new item, the application will automatically generate a unique stock number for the item.
+- If multiple items.
+
+## Issues Found
+Method Restock was not finding the items. `I should've been using `get_all_values()`and not get_all_records()`, 
+
+
+## Acknowledgements
+
+The Inventory Tracker application was developed as a programming school project.
+
+## Future additions
+
+The addition of a UI
--- run.py
+++ run.py
@@ -1,3 +1,165 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+class InventoryTracker:
+    def __init__(self):
+        # Connect to Google Sheets API
+        SCOPE = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive"
+        ]
+
+        CREDS = Credentials.from_service_account_file('credentials.json')
+        SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+        GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+        self.SHEET = GSPREAD_CLIENT.open("inventory")  # Make SHEET an instance variable
+        self.Stock = None
+ 
+        try:
+            self.Stock = self.SHEET.worksheet('Stock')
+        except gspread.WorksheetNotFound as e:
+            print("Worksheet 'Stock' not found in the 'inventory' document.")
+            print("Available worksheet titles:")
+            worksheet_titles = SHEET.worksheet_titles()
+            print(worksheet_titles)
+            return
+
+    def item_exists(self, item_model):
+        # Check if an item already exists in the inventory
+        items = self.Stock.get_all_records()
+        return any(
+            item.get("item_model") == item_model
+            for item in items
+        )
+
+    def insert_item(self, item_model, quantity):
+        # Insert a new item into the inventory
+        if self.item_exists(item_model):
+            print("Item already exists in the inventory.")
+        else:
+            row = [item_model, quantity]
+            try:
+                self.Stock.append_row(row)
+                print("Item added successfully.")
+            except Exception as e:
+                print(f"Error occurred while adding item: {str(e)}")
+
+
+    def retrieve_items(self, item_search):
+        # Get all values from the worksheet
+        data = self.Stock.get_all_values()  # Use self.Stock instead of SHEET.worksheet
+        # Search for the item name in the first column (index 0) and print the matching names and quantities
+        matching_items = [(row[0], row[1]) for row in data if item_search.lower() in row[0].lower()]
+        if matching_items:
+            print("Matching items:")
+            for item_name, quantity in matching_items:
+                print(f"Item: {item_name}, Quantity: {quantity}")
+               
+        else:
+            print("No matching items found.")
+        
+    
+     
+    def update_quantity(self, item_search, quantity_sold):
+        # Update the quantity of an item in the inventory
+        data = self.Stock.get_all_values()
+        matching_items = [(row[0], row[1]) for row in data if item_search.lower() in row[0].lower()]
+        
+        if len(matching_items) > 0:
+            for item_name, quantity in matching_items:
+                transferred_item = item_name
+                transferred_quantity = int(quantity)
+                transferred_quantity -= quantity_sold
+                item_data = [[transferred_item, str(transferred_quantity)]]  # Make item_data a 2D list
+                cell = self.Stock.find(transferred_item)
+                self.Stock.update(cell.address, item_data)
+                print(f"Quantity updated for {transferred_item}: {transferred_quantity}")
+        else:
+            print("Item not found in the inventory.")
+
+
+    def restock_item(self, item_model, additional_quantity):
+        # Restock an item in the inventory
+        try:
+            additional_quantity = int(additional_quantity)
+        except ValueError:
+            print("Invalid additional quantity. Please enter a valid integer value.")
+            return
+
+        data = self.Stock.get_all_values()
+        for index, row in enumerate(data[1:], start=2):  # Start from index 1 to skip the header row
+            if row[0] == item_model:
+                current_quantity = int(row[1])
+                new_quantity = current_quantity + additional_quantity
+                row_number = index
+                cell_range = f"B{row_number}"
+                self.Stock.update(cell_range, str(new_quantity))
+                print("Item restocked successfully.")
+                return
+
+        # Item not found
+        print("Item not found in the inventory.")
+
+
+
+
+
+
+
+ 
+    def run(self):
+        # Main loop for the inventory tracking application
+        while True:
+            print("Choose an operation:")
+            print("1. Search for an item")
+            print("2. Insert a new item")
+            print("3. Update quantity")
+            print("4. Restock item")
+            print("0. Exit")
+
+            choice = int(input("Enter your choice (1, 2, 3, 4, or 0):\n "))
+            if choice == 1:
+                item_search = input("Enter the item model or shorthand code to search:\n ")
+                self.retrieve_items(item_search)
+
+            elif choice == 2:
+                item_model = input("Enter item model:\n ")
+                while True:
+                    quantity = input("Enter initial quantity:\n ")
+                    try:
+                        quantity = int(quantity)
+                        break
+                    except ValueError:
+                        print("Invalid quantity. Please enter a valid integer value.")
+                self.insert_item(item_model, quantity)
+
+            elif choice == 3:
+                item_search = input("Enter the item model to update quantity:\n ")
+                while True:
+                    quantity_sold = input("Enter the quantity sold during the daily sale:\n ")
+                    try:
+                        quantity_sold = int(quantity_sold)
+                        break
+                    except ValueError:
+                        print("Invalid quantity. Please enter a valid integer value.")
+                self.update_quantity(item_search, quantity_sold)
+
+            elif choice == 4:
+                item_model = input("Enter the item model to restock:\n ")
+                additional_quantity = input("Enter the additional quantity to be added:\n ")
+                self.restock_item(item_model, additional_quantity)  # Remove quantity_sold argument
+
+            elif choice == 0:
+                print("Exiting the inventory tracking application.")
+                break
+
+            else:
+                print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    # Create an instance of the InventoryTracker class and run the application
+    tracker = InventoryTracker()
+    tracker.run()
