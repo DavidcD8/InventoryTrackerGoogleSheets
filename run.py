4c25f04067551a3b085b13c9d26d9e0581488f0b
David Cuevas Diaz <91196677+DavidcD8@users.noreply.github.com>
2023-08-10 11:24:02 +0100

    restored deleted insert_item function

--- run.py
+++ run.py
@@ -24,6 +24,32 @@ def is_valid_quantity(quantity):
         return False
 
 
+def insert_item(sheet, item_model, quantity):
+    # Insert a new item into the inventory
+    item_model = item_model.strip()  # Remove leading/trailing spaces
+
+    if not item_model:
+        print("Invalid item model. Please enter a non-empty item model.")
+        return
+
+    if not is_valid_quantity(quantity):
+        print("Invalid quantity. Please enter a valid non-negative integer.")
+        return
+
+    try:
+        # Try to find the item_model in the first column (index 1)
+        cell = sheet.find(item_model, in_column=1)
+        print("Item already exists in the inventory.")
+    except gspread.exceptions.CellNotFound:
+        # The item_model was not found, so it doesn't exist in the inventory
+        row = [item_model, quantity]
+        try:
+            sheet.append_row(row)
+            print("Item added successfully.")
+        except Exception as e:
+            print(f"Error occurred while adding item: {str(e)}")
+
+
 def retrieve_items(sheet, item_search):
     # Get all values from the worksheet
     data = sheet.get_all_values()
